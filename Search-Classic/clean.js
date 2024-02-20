db0a647ee41edc39f6b0550bd9fca2a67994f0e4
Fratopolis <72286540+Fratopolis@users.noreply.github.com>
2024-02-20 09:13:12 -0600

    Update clean.js

--- Search-Classic/clean.js
+++ Search-Classic/clean.js
@@ -1,31 +1,31 @@
 //Removed Google Games
 var LIST = '[class="g mnr-c g-blk"], [class="g wF4fFd g-blk"]'
-//Searched "Snake" Removed Google Feedback and see results about
-LIST = LIST + ', .VjDLd';//.AuP6le, 
-//Searched "Frozen 2" Removed Videos, Trailers, Songs, and Clips
-LIST = LIST + ', .uVMCKf, .JNkvid, .M8OgIe, .LwV4sf';//, .LXqMce, .sfS5Re'
-//Searched "Frozen 2" Added options like picutes and sub sections to results
-LIST = LIST + ', .NFQFxe';
-//Searched "pythons" Removed People also Ask
-LIST = LIST + ', .AuVD, .cUnQKe';
-//Searched "Snake" Removed Related Searches
-LIST = LIST + ', #w3bYAd, .AUiS2';
-//Searched "Snake" Removed Top Stories
-LIST = LIST + ', [class="yG4QQe TBC9ub"]';
-//Searched "Video Games" Removed top scroll bar and popular options and Buying Options
-LIST = LIST + ', .baPFxb, .kSMK2, .sPmWM';
-//Searched "Video Games" Removed "Refine Video Games" options
-LIST = LIST + ', [class="vZFyxc SdYGsb"]';
-//Searched "blake shelton music videos" Removed Links to Videos
-LIST = LIST + ', .dFd2Tb, [jscontroller="pgCXqb"]';// .mnr-c
-//Searched "Frozen 2" This removes right Side bar
-LIST = LIST + ', [role="complementary"]'; // .hSOk2e
 //This removes ADs
 LIST = LIST + ', #tvcap, #bottomads, #bres, [class="TzHB6b cLjAic"]';
 //This removes Feedback Line  and TAB LIST on some pages
 LIST = LIST + ', [class="rpBMYb kno-ftr"], [role="tablist"], .XqFnDf';
 //This removes Google Maps video content "Milk Island"
 LIST = LIST + ', .kPvgOb, .widget-scene-imagery-iframe';
+//Searched "Video Games" Removed "Refine Video Games" options
+LIST = LIST + ', [class="vZFyxc SdYGsb"]';
+//Searched "blake shelton music videos" Removed Links to Videos
+LIST = LIST + ', .dFd2Tb, [jscontroller="pgCXqb"]';// .mnr-c
+//Searched "Snake" Removed Top Stories
+LIST = LIST + ', [class="yG4QQe TBC9ub"]';
+//Searched "Video Games" Removed top scroll bar and popular options and Buying Options
+LIST = LIST + ', .baPFxb, .kSMK2, .sPmWM';
+//Searched "pythons" Removed People also Ask
+LIST = LIST + ', .AuVD, .cUnQKe';
+//Searched "Snake" Removed Related Searches
+LIST = LIST + ', #w3bYAd, .AUiS2, .oIk2Cb';
+//Searched "Frozen 2" Removed Videos, Trailers, Songs, and Clips
+LIST = LIST + ', .uVMCKf, .JNkvid, .M8OgIe, .LwV4sf';//, .LXqMce, .sfS5Re'
+//Searched "Frozen 2" Added options like picutes and sub sections to results
+LIST = LIST + ', .NFQFxe';
+
+//Right side bar
+LIST = LIST + ', .TzHB6b, .sATSHe'
+
 
 //Searched "let it go music" and "Frozen 2 songs" This removes other music/films related content and lists
 LIST = LIST + ', [data-attrid="action:listen_recording_cluster"]';
